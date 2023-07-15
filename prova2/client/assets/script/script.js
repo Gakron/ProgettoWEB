4490373fcb2dbe3af808a08086bfbf64c137fe70
Gakron <carmelo.mannino67@gmail.com>
2023-07-15 19:26:50 +0200

    primo commit

---
+++ prova2/client/assets/script/script.js
@@ -0,0 +1,64 @@
+const form_login = document.getElementById("form_login");
+const form_signup = document.getElementById("form_signup");
+
+const log_to_signup = document.getElementById("log_to_signup");
+const signup_to_log = document.getElementById("signup_to_log");
+
+log_to_signup.addEventListener("click", () => {
+    form_login.classList.add("hidden");
+    form_signup.classList.remove("hidden");
+})
+
+
+signup_to_log.addEventListener("click", () => {
+    form_signup.classList.add("hidden");
+    form_login.classList.remove("hidden");
+})
+
+const register = () =>
+    fetch("http://localhost:3000/register", {
+        method: "POST",
+        body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            confirm_password: document.getElementById("confirm_password").value
+        }),
+        headers: {
+            "Accept": "*/*",
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(res => {
+        if (res.ok)
+            alert("TI SEI REGISTRATO!");
+        else if (res.status == 409)
+            alert("EMAIL GIA REGISTRATA")
+        else if (res.status == 400)
+            alert("LE PASSWORD NON CORRISPONDONO")
+        else
+            alert("GENERIC ERROR")
+    })
+        .catch(_ => {
+            alert("GENERIC ERROR")
+        })
+
+
+const login = () =>
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email: document.getElementById("lgn_email").value,
+            password: document.getElementById("lgn_password").value,
+        }),
+        headers: {
+            "Accept": "*/*",
+            "Content-type": "application/json; charset=UTF-8"
+        }
+
+    }).then(res=>{
+        if(res.ok)
+            window.location.href = "/ciao";
+        else
+            alert(res.statusText);
+    })
+
+
