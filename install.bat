b341acc4d8c0f0f52c2cc40ea377a2c00c9e0ab5
Gakron <carmelo.mannino67@gmail.com>
2023-08-11 00:18:33 +0200

    fine(?)

--- install.bat
+++ install.bat
@@ -1,23 +1,43 @@
 @echo off
 
-:: Verifica se SQLLocalDB è già installato
-sqllocaldb info MSSQLLocalDB > nul 2>&1
-if %errorlevel% neq 0 (
-    echo SQLLocalDB non è installato. Installazione in corso...
-    
-    :: Scarica ed installa SQLLocalDB
-    :: Modifica il percorso del file di installazione di SQLLocalDB
-    start /wait SQL2022-SSEI-Expr.exe
-    
-    echo SQLLocalDB installato.
+REM Verifica se Node.js è già installato
+node -v >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Node.js è già installato.
 ) else (
-    echo SQLLocalDB è già installato.
+    start /wait node-v18.17.1-x64.msi
+)
+
+REM Installa le dipendenze Node.js
+npm install
+
+REM Verifica se MySQL è installato
+mysql --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo MySQL non è installato. Verrà scaricato e installato.
+
+    REM Installa MySQL 
+    start /wait mysql-installer-web-community-8.0.34.0 (1).msi
+
+    REM Aggiungi il percorso di MySQL all'ambiente PATH 
+    setx PATH "%PATH%;C:\Program Files\MySQL\MySQL Server 8.0\bin" /M
 )
 
 
+REM Crea il database MySQL (assumendo che MySQL sia già installato e configurato)
+mysql -u root -pciaociao19 -P 3305 -e "CREATE DATABASE IF NOT EXIST progetto;"
 
-msiexec /i "MsSqlCmdLnUtils.msi" /qn TARGETDIR="%~dp0
+REM Ripristina il backup del database
+mysql -u root -pciaociao19 -P 3305 progetto < backup.sql
 
-echo SQLCMD installato.
+REM Crea il file .env
+copy .env.example .env
 
-:: ...
+REM Modifica il file .env con le credenziali e le configurazioni appropriate
+powershell -Command "(Get-Content .env) -replace 'DB_HOST=localhost', 'DB_HOST=localhost' | Set-Content .env"
+powershell -Command "(Get-Content .env) -replace 'DB_USER=username', 'DB_USER=root' | Set-Content .env"
+powershell -Command "(Get-Content .env) -replace 'DB_PASS=password', 'DB_PASS=ciaociao19' | Set-Content .env"
+powershell -Command "(Get-Content .env) -replace 'DB_PORT=3306', 'DB_PORT=3305' | Set-Content .env"
+
+REM ... 
+pause
