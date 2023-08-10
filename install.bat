cdc2db177c2f195fa2d6ede738067c9733284f1a
Gakron <carmelo.mannino67@gmail.com>
2023-08-10 20:19:59 +0200

    fine(?)

--- install.bat
+++ install.bat
@@ -21,36 +21,3 @@ msiexec /i "MsSqlCmdLnUtils.msi" /qn TARGETDIR="%~dp0
 echo SQLCMD installato.
 
 :: ...
-
-:: Aggiungi la cartella del file batch a PATH (assicurati che la cartella non sia già presente)
-setx PATH "%~dp0;%PATH%"
-
-:: ...
-
-
-echo Creazione del Database Locale in corso...
-:: Configurazione
-set server_name=(localdb)\MSSQLLocalDB
-set database_name=progetto
-set backup_file=backup.sql
-
-:: Cerca sqlcmd.exe nel percorso di sistema
-for %%i in (sqlcmd.exe) do (
-    set "sqlcmd_exe=%%~$PATH:i"
-)
-
-:: Controlla se sqlcmd.exe è stato trovato
-if not defined sqlcmd_exe (
-    echo sqlcmd.exe non trovato nel percorso di sistema.
-    pause
-    exit /b
-)
-
-:: Crea il database
-%sqlcmd_exe% -S %server_name% -Q "CREATE DATABASE %database_name%;"
-
-:: Ripristina il backup
-%sqlcmd_exe% -S %server_name% -d %database_name% -i %backup_file%
-
-echo Creazione completata.
-pause
