8c05f4339f1dc19efb454b9075916d166ea218ce
Gakron <carmelo.mannino67@gmail.com>
2023-08-11 12:56:40 +0200

    fine(?)

--- install.bat
+++ install.bat
@@ -9,19 +9,14 @@ if %errorlevel% equ 0 (
     start /wait msiexec /i node-v18.17.1-x64.msi /quiet
 )
 
-REM Verifica se l'installazione è andata a buon fine
-node -v >nul 2>&1
-if %errorlevel% equ 0 (
-    echo Installazione di Node.js completata.
-) else (
-    echo Si è verificato un problema durante l'installazione di Node.js.
-    pause
-    exit /b 1
-)
+timeout /t 10 /nobreak >nul
+cmd /c
+echo Installazione di Node.js completata.
 
 REM Installa le dipendenze Node.js
 echo Installazione delle dipendenze Node.js...
 npm install
+
 
 REM Verifica se l'installazione delle dipendenze è andata a buon fine
 if %errorlevel% equ 0 (
@@ -32,10 +27,8 @@ if %errorlevel% equ 0 (
     exit /b 1
 )
 
-REM Attendi alcuni secondi per permettere al server di avviarsi completamente
-echo Attendi che il server si avvii...
-timeout /t 10 /nobreak >nul
 
-echo Avvio completato. Premi un tasto per chiudere lo script.
+
+echo Completato. Premi un tasto per chiudere lo script.
 pause
-pause
+
