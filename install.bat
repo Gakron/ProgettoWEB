be9b26d217ce47fe800e2f98b772166d75bea2d7
Gakron <carmelo.mannino67@gmail.com>
2023-08-11 12:28:50 +0200

    fine(?)

--- install.bat
+++ install.bat
@@ -1,11 +1,9 @@
-@echo off
-
 REM Verifica se Node.js è già installato
 node -v >nul 2>&1
 if %errorlevel% equ 0 (
     echo Node.js è già installato.
 ) else (
-    start /wait node-v18.17.1-x64.msi
+    start /wait msiexec /i node-v18.17.1-x64.msi /quiet
 )
 
 REM Installa le dipendenze Node.js
