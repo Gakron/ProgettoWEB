127a9e6f5b94ae04dd0ce8de673eb9947a198f72
Gakron <carmelo.mannino67@gmail.com>
2023-08-11 13:28:35 +0200

    fine(?)

--- install.bat
+++ install.bat
@@ -1,23 +1,13 @@
 @echo off
-setlocal
 
 REM Controlla se Node.js è installato
 where node > nul 2>&1
-if %errorlevel% equ 0 (
-    echo Node.js è già installato.
-) else (
-    echo Node.js non è installato nel sistema.
-    set /p installNode=Desideri installare Node.js? (Y/N):
-    if /i "%installNode%"=="Y" (
-        REM Esegui il processo di installazione di Node.js
-        REM Puoi modificare questo comando con l'installazione corretta per il tuo sistema
-        REM Ad esempio, scaricare l'installer da https://nodejs.org/ e modificarlo di conseguenza
-        echo Installazione di Node.js...
-        start /wait msiexec /i node-v18.17.1-x64.msi /qn
-    ) else (
-        echo Installazione di Node.js annullata.
-        exit /b
-    )
+if %errorlevel% neq 0 (
+    REM Esegui il processo di installazione di Node.js
+    REM Puoi modificare questo comando con l'installazione corretta per il tuo sistema
+    REM Ad esempio, scaricare l'installer da https://nodejs.org/ e modificarlo di conseguenza
+    echo Installazione di Node.js...
+    start /wait msiexec /i https://nodejs.org/dist/v14.17.6/node-v14.17.6-x64.msi /qn
 )
 
 REM Esegui npm install per scaricare le dipendenze
